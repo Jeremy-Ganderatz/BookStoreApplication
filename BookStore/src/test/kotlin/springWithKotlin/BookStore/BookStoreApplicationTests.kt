b6cf222a5f5415e636443f7cfc6df706dc83d3cf
Jérémy Ganderatz <jeremyganderatz@macbook-air-de-jeremy.home>
2023-10-20 14:22:28 +0200

    setting up spring and test application

---
+++ BookStore/src/test/kotlin/springWithKotlin/BookStore/BookStoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package springWithKotlin.BookStore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
